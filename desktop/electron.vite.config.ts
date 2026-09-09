2808c0c04900af01beb938e04b9bfc832be6a7dd
SayedAfrasS <sayedafras12@gmail.com>
2026-09-09 16:51:55 +0530

    setup desktop backend

--- desktop/electron.vite.config.ts
+++ desktop/electron.vite.config.ts
@@ -1,16 +1,9 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  main: {},
+  main: { plugins: [externalizeDepsPlugin()] },
   preload: {},
-  renderer: {
-    resolve: {
-      alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
-    },
-    plugins: [react()]
-  }
+  renderer: { plugins: [react(), tailwindcss()] },
 })
